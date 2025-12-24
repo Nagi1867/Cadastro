96708ae296622dc4a70697c0656806f8730b9d92
Gabriel Kiyoto <120345849+Nagi1867@users.noreply.github.com>
2025-12-24 15:20:14 -0300

    componente tabela

--- src/pages/index.jsx
+++
@@ -1,7 +0,0 @@
-import Link from "next/link";
-
-export default function Home() {
-  return (
-    <div className="bg-red-700">teste</div>
-  );
-}
---
+++ src/pages/index.tsx
@@ -0,0 +1,30 @@
+import Link from "next/link";
+import Layout from "../components/Layout";
+import Tabela from "../components/Tabela";
+import Cliente from "../core/Cliente";
+
+
+export default function Home() {
+  const clientes = [
+    new Cliente('Ana', 34, '1'),
+    new Cliente('Bia', 45, '2'),
+    new Cliente('Pedro', 23, '3'),
+    new Cliente('Carlos', 54, '4')
+  ]
+
+  function clienteSelecionado(cliente: Cliente) {
+
+  }
+
+    function clienteExcluido(cliente: Cliente) {
+
+  }
+
+  return (
+    <div className="flex h-screen justify-center items-center bg-gradient-to-r from-purple-500 to-blue-600 text-white">
+      <Layout titulo="Cadastro Titulo">
+        <Tabela clientes={clientes} clienteSelecionado={clienteSelecionado} clienteExcluido={clienteExcluido}></Tabela>
+      </Layout>
+    </div>
+  );
+}
